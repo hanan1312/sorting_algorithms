2011d5a6f440294efa1308123a8f6d2f4f28b743
helmogey <h.elmogey@gmail.com>
2024-02-20 20:32:52 +0200

    1st

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,7 +8,9 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-int i, j, temp;
+size_t i, j, k;
+int temp;
+
 for (i = 0; i < size - 1; i++)
 {
 for (j = 0; j < size - i - 1; j++)
@@ -20,7 +22,7 @@ array[j] = array[j + 1];
 array[j + 1] = temp;
 
 printf("Array after swapping %d and %d: ", array[j], array[j + 1]);
-for (int k = 0; k < size; k++)
+for (k = 0; k < size; k++)
 {
 printf("%d ", array[k]);
 }
