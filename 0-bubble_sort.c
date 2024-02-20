4550ce37b5d1a05ee3ac7b1f6d7727fad5aba5c3
hanan1312 <nana.elmogey@gmail.com>
2024-02-21 01:16:48 +0200

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,22 +6,28 @@
  * @array: The array to be sorted.
  * @size: The size of the array.
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-			}
-		}
-	}
+size_t i, j, k;
+int temp;
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+for (k = 0; k < size - 1; k++)
+{
+printf("%d, ", array[k]);
 }
+printf("%d\n", array[k]);
+}
+}
+}
+}
+
 
