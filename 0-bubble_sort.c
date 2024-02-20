33967cf884dc6bdf285eed70de53e6269b5c2c17
hanan1312 <nana.elmogey@gmail.com>
2024-02-21 01:15:45 +0200

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,27 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Entry point
- * @array: size_t
- * @size: size_t
- * Return: void
+ * bubble_sort - Sorts an array of integers in ascending order using
+ *               the Bubble Sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j, k;
-int temp;
+	size_t i, j;
+	int temp;
 
-for (i = 0; i < size - 1; i++)
-{
-for (j = 0; j < size - i - 1; j++)
-{
-if (array[j] > array[j + 1])
-{
-temp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = temp;
-for (k = 0; k < size - 1; k++)
-{
-printf("%d, ", array[k]);
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
 }
-printf("%d\n", array[k]);
-}
-}
-}
-}
+
