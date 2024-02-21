db352f1c9ffa84371555754456885d2eb6bde09f
helmogey <h.elmogey@gmail.com>
2024-02-21 11:35:43 +0200

    fix select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,10 +8,10 @@
  */
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, min_idx;
+size_t i, j, min_idx, k;
 int temp;
 
-for (i = 0; i < size - 3; i++)
+for (i = 0; i < size - 1; i++)
 {
 min_idx = i;
 for (j = i + 1; j < size; j++)
